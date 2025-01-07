67104334ca71267caf2f42af785a8950434507de
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-01-08 05:59:23 +0600

    Add

--- test.py
+++
@@ -1,9 +0,0 @@
-import ipaddress
-import struct
-
-
-# Example usage
-if __name__ == "__main__":
-    addrs = ["10.10.10.5:128", "100.56.58.99:28525"]
-    compact = addrs_to_compact(addrs)
-    print(compact)
