f5ba6d517c2eb2fd5b9901aa80d9aab536ce2e69
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-01-10 11:52:14 +0600

    Add

--- src/coreproject_tracker/servers/udp.py
+++ src/coreproject_tracker/servers/udp.py
@@ -1,6 +1,7 @@
 import json
 import struct
 
+from twisted.internet import threads
 from twisted.internet.protocol import DatagramProtocol
 from twisted.logger import Logger
 
@@ -30,7 +31,129 @@ class UDPServer(DatagramProtocol):
     def __init__(self):
         super().__init__()
 
-    def make_udp_packet(self, params: dict[str, int | bytes | dict]) -> bytes:
+    def datagramReceived(self, data, addr):
+        deferred = threads.deferToThread(self._datagramReceived, data, addr)
+        deferred.addCallback(self.on_task_done, addr)
+        deferred.addErrback(self.on_task_error, addr)
+
+        # res = self._datagramReceived(data, addr)
+        # self.on_task_done(res, addr)
+
+    def on_task_done(self, result, addr):
+        self.transport.write(result, addr)
+
+    def on_task_error(self, failure, addr):
+        print(failure)
+        self.transport.write(failure.getErrorMessage(), addr)
+
+    def _datagramReceived(self, data, addr):
+        """
+        Called when a datagram (UDP packet) is received.
+
+        - `data`: The received message.
+        - `addr`: The address of the sender (tuple of IP and port).
+        """
+        if (packet_length := len(data)) < 16:
+            log.error(
+                f"received packet length is {packet_length} is shorter than 16 bytes"
+            )
+
+        param = self.parse_udp_packet(data, addr)
+
+        if param["action"] == ACTIONS.ANNOUNCE:
+            hset_with_ttl(
+                param["info_hash"],
+                param["peer_id"],
+                json.dumps(
+                    {
+                        "peer_id": param["peer_id"],
+                        "info_hash": param["info_hash"],
+                        "peer_ip": param["ip"],
+                        "port": param["port"],
+                        "left": param["left"],
+                    }
+                ),
+                PEER_TTL,
+            )
+
+            peer_count = 0
+            peers = []
+            seeders = 0
+            leechers = 0
+
+            redis_data = hget_all_with_ttl(param["info_hash"])
+            peers_list = redis_data.values()
+
+            for peer in peers_list:
+                if peer_count > param["numwant"]:
+                    break
+                peer_data = json.loads(peer)
+
+                if peer_data["left"] == 0:
+                    seeders += 1
+                else:
+                    leechers += 1
+
+                peers.append(f"{peer_data['peer_ip']}:{peer_data['port']}")
+                peer_count += 1
+
+            param["peers"] = addrs_to_compact(peers)
+            param["complete"] = seeders
+            param["incomplete"] = leechers
+            param["interval"] = ANNOUNCE_INTERVAL
+
+        res = self.make_udp_packet(param)
+        return res
+
+    def parse_udp_packet(self, msg, addr):
+        connection_id = msg[:8]
+        connection_id_unpacked = struct.unpack(">Q", msg[:8])[0]
+        if connection_id_unpacked != CONNECTION_ID:
+            raise ValueError(
+                f"'{connection_id_unpacked}' is not same as {CONNECTION_ID}"
+            )
+
+        action = from_uint32(msg[8:12])
+        transaction_id = from_uint32(msg[12:16])
+
+        # Construct the result (similar to the JavaScript object)
+        params = {
+            "connection_id": connection_id,
+            "action": action,
+            "transaction_id": transaction_id,
+            "type": "udp",
+        }
+
+        if params["action"] == ACTIONS.ANNOUNCE:
+            params["info_hash"] = msg[16:36].hex()  # 20 bytes
+            params["peer_id"] = msg[36:56].hex()  # 20 bytes
+            params["downloaded"] = from_uint64(
+                msg[56:64]
+            )  # Convert 64-bit unsigned integer
+            params["left"] = from_uint64(msg[64:72])  # Convert 64-bit unsigned integer
+            params["uploaded"] = from_uint64(
+                msg[72:80]
+            )  # Convert 64-bit unsigned integer
+
+            # Read 4-byte unsigned int (big-endian)
+            event_id = struct.unpack(">I", msg[80:84])[0]
+            params["event"] = EVENTS.get(event_id)
+            if not params["event"]:
+                raise ValueError("Invalid event")
+
+            params["ip"] = from_uint32(msg[84:88]) or addr[0]
+            params["key"] = from_uint32(msg[88:92])
+
+            params["numwant"] = min(
+                from_uint32(msg[92:96]) or DEFAULT_ANNOUNCE_PEERS, MAX_ANNOUNCE_PEERS
+            )
+            params["port"] = from_uint16(msg[96:98]) or addr[1]
+            params["addr"] = f"{params['ip']}:{params['port']}"
+            params["compact"] = 1
+        return params
+
+    @staticmethod
+    def make_udp_packet(params: dict[str, int | bytes | dict]) -> bytes:
         """
         Create UDP packets for BitTorrent tracker protocol.
 
@@ -99,107 +222,3 @@ class UDPServer(DatagramProtocol):
             raise ValueError(f"Action not implemented: {action}")
 
         return packet
-
-    def parse_udp_packet(self, msg, addr):
-        connection_id = msg[:8]
-        connection_id_unpacked = struct.unpack(">Q", msg[:8])[0]
-        if connection_id_unpacked != CONNECTION_ID:
-            raise ValueError("Error")
-
-        action = from_uint32(msg[8:12])
-        transaction_id = from_uint32(msg[12:16])
-
-        # Construct the result (similar to the JavaScript object)
-        params = {
-            "connection_id": connection_id,
-            "action": action,
-            "transaction_id": transaction_id,
-            "type": "udp",
-        }
-
-        if params["action"] == ACTIONS.ANNOUNCE:
-            params["info_hash"] = msg[16:36].hex()  # 20 bytes
-            params["peer_id"] = msg[36:56].hex()  # 20 bytes
-            params["downloaded"] = from_uint64(
-                msg[56:64]
-            )  # Convert 64-bit unsigned integer
-            params["left"] = from_uint64(msg[64:72])  # Convert 64-bit unsigned integer
-            params["uploaded"] = from_uint64(
-                msg[72:80]
-            )  # Convert 64-bit unsigned integer
-
-            # Read 4-byte unsigned int (big-endian)
-            event_id = struct.unpack(">I", msg[80:84])[0]
-            params["event"] = EVENTS.get(event_id)
-            if not params["event"]:
-                raise ValueError("Invalid event")
-
-            params["ip"] = from_uint32(msg[84:88]) or addr[0]
-            params["key"] = from_uint32(msg[88:92])
-
-            params["numwant"] = min(
-                from_uint32(msg[92:96]) or DEFAULT_ANNOUNCE_PEERS, MAX_ANNOUNCE_PEERS
-            )
-            params["port"] = from_uint16(msg[96:98]) or addr[1]
-            params["addr"] = f"{params['ip']}:{params['port']}"
-            params["compact"] = 1
-        return params
-
-    def datagramReceived(self, data, addr):
-        """
-        Called when a datagram (UDP packet) is received.
-
-        - `data`: The received message.
-        - `addr`: The address of the sender (tuple of IP and port).
-        """
-        if (packet_length := len(data)) < 16:
-            log.error(
-                f"received packet length is {packet_length} is shorter than 16 bytes"
-            )
-
-        param = self.parse_udp_packet(data, addr)
-
-        if param["action"] == ACTIONS.ANNOUNCE:
-            hset_with_ttl(
-                data["info_hash"],
-                data["peer_id"],
-                json.dumps(
-                    {
-                        "peer_id": data["peer_id"],
-                        "info_hash": data["info_hash"],
-                        "peer_ip": data["peer_ip"],
-                        "port": data["port"],
-                        "left": data["left"],
-                    }
-                ),
-                PEER_TTL,
-            )
-
-            peer_count = 0
-            peers = []
-            seeders = 0
-            leechers = 0
-
-            redis_data = hget_all_with_ttl(data["info_hash"])
-            peers_list = redis_data.values()
-
-            for peer in peers_list:
-                if peer_count > param["numwant"]:
-                    break
-                peer_data = json.loads(peer)
-
-                if peer_data["left"] == 0:
-                    seeders += 1
-                else:
-                    leechers += 1
-
-                peers.append(f"{peer_data['peer_ip']}:{peer_data['port']}")
-                peer_count += 1
-
-            param["peers"] = addrs_to_compact(peers)
-            param["complete"] = seeders
-            param["incomplete"] = leechers
-            param["interval"] = ANNOUNCE_INTERVAL
-
-        res = self.make_udp_packet(param)
-        self.transport.write(res, addr)
