51a7d888762137eca965b0f2e764be83ef4c2f5a
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-01-10 17:45:04 +0600

    add

--- src/coreproject_tracker/servers/websocket.py
+++ src/coreproject_tracker/servers/websocket.py
@@ -1,6 +1,7 @@
 import json
 
 from autobahn.twisted.websocket import WebSocketServerProtocol
+from twisted.internet import reactor, threads
 
 from coreproject_tracker.constants import (
     MAX_ANNOUNCE_PEERS,
@@ -17,6 +18,106 @@ from coreproject_tracker.manager import ConnectionManager
 
 class WebSocketServer(WebSocketServerProtocol):
     connection_manager = ConnectionManager()
+
+    def onMessage(self, payload, isBinary):
+        """
+        Called when a message is received from the client.
+        """
+        threads.deferToThread(self.__onMessage, payload, isBinary)
+
+    def __sendMessage(self, message):
+        reactor.callFromThread(self.sendMessage, message)
+
+    def __onMessage(self, payload, isBinary):
+        payload = payload.decode("utf8") if not isBinary else payload
+        params = json.loads(payload)
+        try:
+            data = self.parse_websocket(params)
+        except ValueError as e:
+            self.sendMessage(
+                json.dumps(
+                    {
+                        "failure reason": e,
+                    }
+                ),
+                isBinary,
+            )
+
+        response = {}
+        response["action"] = data["action"]
+        hset_with_ttl(
+            data["info_hash"],
+            data["addr"],
+            json.dumps(
+                {
+                    "peer_id": data["peer_id"],
+                    "info_hash": data["info_hash"],
+                    "peer_ip": data["peer_ip"],
+                    "port": data["port"],
+                    "left": data["left"],
+                }
+            ),
+        )
+
+        seeders = 0
+        leechers = 0
+
+        redis_data = hget_all_with_ttl(data["info_hash"])
+        peers_list = [json.loads(peer) for peer in redis_data.values()]
+
+        for peer in peers_list:
+            if peer["left"] == 0:
+                seeders += 1
+            else:
+                leechers += 1
+
+        response["completed"] = seeders
+        response["incompleted"] = leechers
+
+        if response["action"] == "announce":
+            response["info_hash"] = hex_to_bin(params["info_hash"])
+            response["interval"] = WEBSOCKET_INTERVAL
+            self.__sendMessage(json.dumps(response).encode(), isBinary)
+
+        if not params.get("answer"):
+            self.__sendMessage(json.dumps(response).encode(), isBinary)
+
+        self.connection_manager.add_connection(data["peer_id"], self)
+
+        if (offers := params.get("offers")) and isinstance(offers, list):
+            for index, peer in enumerate(peers_list):
+                peer_instance = self.connection_manager.get_connection(peer["peer_id"])
+                peer_instance.sendMessage(
+                    json.dumps(
+                        {
+                            "action": "announce",
+                            "offer": params["offers"][index]["offer"],
+                            "offer_id": params["offers"][index]["offer_id"],
+                            "peer_id": hex_to_bin(params["peer_id"]),
+                            "info_hash": hex_to_bin(params["info_hash"]),
+                        }
+                    ).encode(),
+                    isBinary,
+                )
+
+        if params.get("answer"):
+            to_peer = self.connection_manager.get_connection(
+                bin_to_hex(data["to_peer_id"])
+            )
+
+            if to_peer:
+                to_peer.sendMessage(
+                    json.dumps(
+                        {
+                            "action": "announce",
+                            "answer": params["answer"],
+                            "offer_id": params["offer_id"],
+                            "peer_id": hex_to_bin(params["peer_id"]),
+                            "info_hash": hex_to_bin(params["info_hash"]),
+                        }
+                    ).encode(),
+                    isBinary,
+                )
 
     def parse_websocket(self, params={}):
         params["type"] = "ws"
@@ -72,94 +173,3 @@ class WebSocketServer(WebSocketServerProtocol):
         params["port"] = client_port
         params["addr"] = f"{client_ip}:{client_port}"
         return params
-
-    def onMessage(self, payload, isBinary):
-        payload = payload.decode("utf8") if not isBinary else payload
-        params = json.loads(payload)
-        try:
-            data = self.parse_websocket(params)
-        except ValueError as e:
-            self.sendMessage(
-                json.dumps(
-                    {
-                        "failure reason": e,
-                    }
-                ),
-                isBinary,
-            )
-
-        response = {}
-        response["action"] = data["action"]
-        hset_with_ttl(
-            data["info_hash"],
-            data["addr"],
-            json.dumps(
-                {
-                    "peer_id": data["peer_id"],
-                    "info_hash": data["info_hash"],
-                    "peer_ip": data["peer_ip"],
-                    "port": data["port"],
-                    "left": data["left"],
-                }
-            ),
-        )
-
-        seeders = 0
-        leechers = 0
-
-        redis_data = hget_all_with_ttl(data["info_hash"])
-        peers_list = [json.loads(peer) for peer in redis_data.values()]
-
-        for peer in peers_list:
-            if peer["left"] == 0:
-                seeders += 1
-            else:
-                leechers += 1
-
-        response["completed"] = seeders
-        response["incompleted"] = leechers
-
-        if response["action"] == "announce":
-            response["info_hash"] = hex_to_bin(params["info_hash"])
-            response["interval"] = WEBSOCKET_INTERVAL
-            self.sendMessage(json.dumps(response).encode(), isBinary)
-
-        if not params.get("answer"):
-            self.sendMessage(json.dumps(response).encode(), isBinary)
-
-        self.connection_manager.add_connection(data["peer_id"], self)
-
-        if (offers := params.get("offers")) and isinstance(offers, list):
-            for index, peer in enumerate(peers_list):
-                peer_instance = self.connection_manager.get_connection(peer["peer_id"])
-                peer_instance.sendMessage(
-                    json.dumps(
-                        {
-                            "action": "announce",
-                            "offer": params["offers"][index]["offer"],
-                            "offer_id": params["offers"][index]["offer_id"],
-                            "peer_id": hex_to_bin(params["peer_id"]),
-                            "info_hash": hex_to_bin(params["info_hash"]),
-                        }
-                    ).encode(),
-                    isBinary,
-                )
-
-        if params.get("answer"):
-            to_peer = self.connection_manager.get_connection(
-                bin_to_hex(data["to_peer_id"])
-            )
-
-            if to_peer:
-                to_peer.sendMessage(
-                    json.dumps(
-                        {
-                            "action": "announce",
-                            "answer": params["answer"],
-                            "offer_id": params["offer_id"],
-                            "peer_id": hex_to_bin(params["peer_id"]),
-                            "info_hash": hex_to_bin(params["info_hash"]),
-                        }
-                    ).encode(),
-                    isBinary,
-                )
