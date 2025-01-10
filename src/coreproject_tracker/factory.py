6f45ef5018d1e1f152d1a2981fca412935ba10be
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-01-10 13:07:17 +0600

    Enable ipv6 listening

--- src/coreproject_tracker/factory.py
+++ src/coreproject_tracker/factory.py
@@ -18,17 +18,21 @@ def make_app(udp_port=9000, http_port=8000, websocket_port=8080):
     globalLogPublisher.addObserver(console_observer)
 
     RedisConnectionManager.initialize()
+
     # UDP Server
-    reactor.listenUDP(udp_port, UDPServer())
+    reactor.listenUDP(udp_port, UDPServer(), interface="127.0.0.1")
+    reactor.listenUDP(udp_port, UDPServer(), interface="::")
 
     # HTTP Server
     root = HTTPServer()
     http_site = Site(root)
-    reactor.listenTCP(http_port, http_site)
+    reactor.listenTCP(http_port, http_site, interface="127.0.0.1")  # IPV4
+    reactor.listenTCP(http_port, http_site, interface="::")  # IPV6
 
     # WebSocket Server
     factory = WebSocketServerFactory()
     factory.protocol = WebSocketServer
-    reactor.listenTCP(websocket_port, factory)
+    reactor.listenTCP(websocket_port, factory, interface="127.0.0.1")  # IPV4
+    reactor.listenTCP(websocket_port, factory, interface="::")  # IPV6
 
     return reactor
