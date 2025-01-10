a35fe44badddeeceb6d85cc99432f70c03254fee
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-01-10 12:06:09 +0600

    Fix error

--- src/coreproject_tracker/constants/ttl.py
+++ src/coreproject_tracker/constants/ttl.py
@@ -1,4 +1,4 @@
 from datetime import timedelta
 
-PEER_TTL = timedelta(hours=1).total_seconds()
-CONNECTION_TTL = timedelta(minutes=5).total_seconds()
+PEER_TTL = int(timedelta(hours=1).total_seconds())
+CONNECTION_TTL = int(timedelta(minutes=5).total_seconds())
